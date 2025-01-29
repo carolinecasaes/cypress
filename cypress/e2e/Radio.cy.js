fd9d6649984a2fc5fefba1b94364cd49bbd5c66a
carolinecasaes <caroline.casaes@outlook.com>
2025-01-28 22:37:22 -0300

    novos progressos

---
+++ cypress/e2e/Radio.cy.js
@@ -0,0 +1,16 @@
+describe('Radio Buttons', () => {
+
+    beforeEach(() => {
+        cy.goHome()
+
+        cy.login('papito@cyskills.com.br', 'showtime')
+        cy.userLoggedIn()
+
+        cy.goTo('/radio', 'Radio Buttons')
+    })
+
+    it('Deve marcar o framework usado no Curso Cypress Skills', () => {
+        cy.contains('label', 'Cypress')
+            .click()
+    })
+})
